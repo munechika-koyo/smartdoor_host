8cb760a542a93d9e82a4b3e70ac5413bcc9105a3
Koyo MUNECHIKA <51052381+munechika-koyo@users.noreply.github.com>
2023-07-30 09:56:05 +0000

    🐛 fix typos

--- service/purge.sh
+++ service/purge.sh
@@ -5,9 +5,9 @@ sudo systemctl stop smartdoor_host.service
 sudo systemctl disable smartdoor_host.service
 
 # purge database backup timer
-sudo systemctl stop smartdoor_host_backup.timer
-sudo systemctl disable smartdoor_host_backup.timer
-sudo systemctl stop smartdoor_host_backup.service
-sudo systemctl disable smartdoor_host_backup.service
+sudo systemctl stop smartdoor_backup.timer
+sudo systemctl disable smartdoor_backup.timer
+sudo systemctl stop smartdoor_backup.service
+sudo systemctl disable smartdoor_backup.service
 
 sudo systemctl daemon-reload
--- service/register.sh
+++ service/register.sh
@@ -6,7 +6,7 @@ sudo systemctl daemon-reload
 sudo systemctl enable smartdoor_host.service
 
 # register database backup timer
-sudo systemctl link $(pwd)/smartdoor_host_backup.service
-sudo systemctl link $(pwd)/smartdoor_host_backup.timer
+sudo systemctl link $(pwd)/smartdoor_backup.service
+sudo systemctl link $(pwd)/smartdoor_backup.timer
 sudo systemctl daemon-reload
-sudo systemctl enable smartdoor_host_backup.timer
+sudo systemctl enable smartdoor_backup.timer
